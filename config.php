03402cf491afb3bee5777cb5b93a37efe23cb49a
lleti <lletibrig@gmail.com>
2016-05-20 15:49:50 +0100

    adding base config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+class config
+{
+    private $username;
+    private $password;
+    private $subreddits = array("KotakuInAction", "KiAChatroom");
+
+    function __construct() {
+    
+    }
+}
