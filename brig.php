8aab6c902b4c61c53b84fbdfba4d164b14206665
lleti <lletibrig@gmail.com>
2016-05-20 16:02:15 +0100

    removing config.php, will replace with a sample.config

---
+++ brig.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once("reddit.php");
+require_once("config.php");
+
+$reddit = new Reddit("USER", "PASS", "CLIENT_ID", "CLIENT_SECRET");
+
+?>
--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 class config
 {
-    private $username;
-    private $password;
+    public $username = ;
+    public $password = "75695wDXP6{Dj1q";
+    public $app_key;
+    public $app_password;
+
     private $subreddits = array("KotakuInAction", "KiAChatroom");
 
     function __construct() {
-    
+
     }
 }
